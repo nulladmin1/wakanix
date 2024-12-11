07d78b3467421b3afa140f0c85be7ebea83a376d
Shrey Deogade <shrey.deogade@protonmail.com>
2024-12-11 14:56:58 -0500

    Replace all the settings options (which took me a while) with a plain attrset for settings

--- modules/wakanix.nix
+++ modules/wakanix.nix
@@ -14,73 +14,17 @@
         description = "Path to .wakatime.cfg";
       };
 
-      settings = {
-        api_url = lib.mkOption {
-          type = lib.types.str;
-          default = "https://api.wakatime.com/api/v1";
-          description = "The url for Wakatime";
-        };
-
-        api_key = lib.mkOption {
-          type = lib.types.str;
-          default = "";
-          description = "The key for Wakatime";
-        };
-        debug = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = "Enable debug (false by default)";
-        };
-
-        hide_file_names = lib.mkOption {
-          type = 
-            with lib.types;
-            oneOf [
-              bool
-              list
-            ];
-          default = false;
-          description = "Obfuscate filenames. Will not send file names to api.";
-        };
-
-        hide_project_names = lib.mkOption {
-          type = 
-            with lib.types;
-            oneOf [
-              bool
-              list
-            ];
-          default = false;
-          description = ''Obfuscate project names. 
-                          When a project folder is detected instead of using the folder name as the project, 
-                          a .wakatime-project file is created with a random project name.'';
-        };
-
-        hide_branch_names = lib.mkOption {
-          type = 
-            with lib.types;
-            oneOf [
-              bool
-              list
-            ];
-          default = false;
-          description = ¨Obfuscate branch names. Will not send revision control branch names to api.¨;
-        };
-        
-        hide_project_folder = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = "Hide project folder";
-        };
-        
-      };
-      extraSettings = lib.mkOption {
+      settings = lib.mkOption {
         type = lib.types.attrsOf lib.types.anything;
-        default = {};
-        description = "Other settings options";
+        default = {
+          debug = false;
+          api_url = "https://api.wakatime.com/api/v1";
+          heartbeat_rate_limit_seconds = 120;
+        };
+        description = "Options for the settings table";
       };
 
-      extraConf = lib.mkOption {
+      config = lib.mkOption {
         type = lib.types.attrsOf lib.types.anything;
         default = {};
         description = "Other configuration options";
@@ -93,7 +37,7 @@
   in
     lib.mkIf cfg.enable {
       home.file."${cfg.configFilePath}".text = lib.generators.toINI {} {
-        settings = cfg.settings // cfg.extraSettings;
+        settings = cfg.settings
       } // cfg.extraConf;
     };
 }
